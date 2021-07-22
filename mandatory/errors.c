521032694c2db5f224bb7255f3629ec5f7da035f
madebypixel02 <madebypixel02@gmail.com>
2021-07-22 22:42:50 +0200

    Small changes

--- mandatory/errors.c
+++ mandatory/errors.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 18:28:25 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/22 18:39:08 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/22 22:42:39 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int	error_msg(char *msg, char *color)
 {
 	if (!color)
 		color = DEFAULT;
-	printf("%sError\n%s%s\n", color, msg, DEFAULT);
+	printf("Error\n%s%s%s\n", color, msg, DEFAULT);
 	return (0);
 }
