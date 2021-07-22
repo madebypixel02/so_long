d5e140e83d984fb845b1c0fd9ca364ccfd07d2a7
madebypixel02 <madebypixel02@gmail.com>
2021-07-22 21:14:15 +0200

    Added initial test file and main

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 16:49:08 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/22 18:41:51 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/22 20:42:56 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@
 int	main(int argc, char **argv)
 {
 	int	fd;
-
+	
 	if (argc != 2)
 		return (error_msg("Invalid number of arguments!", RED));
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 		return (error_msg("File not found!", RED));
-	printf("Line: %s\n", get_next_line(fd));
+	if (ft_strrncmp(argv[1], ".ber", 4))
+		return (error_msg("Invalid file type, use .ber!", RED));
 	return (0);
 }
