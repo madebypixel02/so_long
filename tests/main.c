48042ff0342d6c6727bafff9e323638beaf871c2
madebypixel02 <madebypixel02@gmail.com>
2021-07-22 22:41:42 +0200

    Tweaked test stuff

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 16:49:08 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/22 20:42:56 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/22 22:37:26 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 int	main(int argc, char **argv)
 {
 	int	fd;
-	
+
 	if (argc != 2)
 		return (error_msg("Invalid number of arguments!", RED));
 	fd = open(argv[1], O_RDONLY);
@@ -25,5 +25,6 @@ int	main(int argc, char **argv)
 		return (error_msg("File not found!", RED));
 	if (ft_strrncmp(argv[1], ".ber", 4))
 		return (error_msg("Invalid file type, use .ber!", RED));
+	return (ft_checklayout(fd));
 	return (0);
 }
