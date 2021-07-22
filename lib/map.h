f3b7523ca8d092a53563091e35c4338a38eeca3e
madebypixel02 <madebypixel02@gmail.com>
2021-07-22 22:41:13 +0200

    Added struct for layout parsing

--- lib/map.h
+++ lib/map.h
@@ -6,14 +6,23 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 19:36:06 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/22 19:51:44 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/22 22:37:10 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef MAP_H
+#ifndef MAP_H
 
 # define MAP_H
 
+typedef struct s_layout
+{
+	int	nrow;
+	int	ncol;
+	int	nexits;
+	int	nplayers;
+}				t_layout;
 
+void	ft_newlayout(t_layout *lay);
+int		ft_checklayout(int fd);
 
 #endif
