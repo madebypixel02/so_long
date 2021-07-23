9dc48f9d7492fddf2d4b6aeb915c7aa37f4efea8
madebypixel02 <madebypixel02@gmail.com>
2021-07-23 20:06:16 +0200

    [WIP] Added struct to manage errors in the opened map

--- lib/errors.h
+++ lib/errors.h
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 18:26:18 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/23 09:51:36 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/23 19:14:49 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@
 
 # include "colors.h"
 # include <stdio.h>
+
+typedef struct s_map_error
+{
+	int	inv_rowlen;
+	int	inv_char;
+	int	inv_nplayers;
+	int	inv_nexits;
+	int	inv_borders;
+}				t_map_error;
 
 int		error_msg(char *msg, char *color, char **to_free);
 void	free_matrix(char **to_free);
