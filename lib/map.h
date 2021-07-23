316519d31d6048d9cae0b763e0c0eb8ff8eda94e
madebypixel02 <madebypixel02@gmail.com>
2021-07-23 20:11:58 +0200

    Misc

--- lib/map.h
+++ lib/map.h
@@ -6,13 +6,15 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 19:36:06 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/23 09:44:33 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/23 19:59:57 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_H
 
 # define MAP_H
+
+# include "errors.h"
 
 typedef struct s_layout
 {
@@ -23,7 +25,8 @@ typedef struct s_layout
 }				t_layout;
 
 void	ft_newlayout(t_layout *lay);
-char	**ft_readlayout(int fd);
-int		ft_checklayout(char **layout);
+void	ft_newmap_error(t_map_error *map_error);
+int		ft_print_map_error(t_map_error *map_error);
+int		ft_readlayout(int fd, t_map_error *map_error);
 
 #endif
