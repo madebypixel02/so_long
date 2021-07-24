aac29ee7d24fed874d27f93706a3c593847aa700
madebypixel02 <madebypixel02@gmail.com>
2021-07-24 21:20:22 +0200

    Renamed structs (norminette reasons)

--- lib/errors.h
+++ lib/errors.h
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 18:26:18 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/24 13:31:05 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/24 20:53:43 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@
 # include "colors.h"
 # include <stdio.h>
 
-typedef struct s_map_error
+typedef struct s_map_err
 {
 	int	inv_rowlen;
 	int	inv_char;
 	int	inv_nplayers;
-	int	inv_ncollectibles;
+	int	inv_ncollect;
 	int	inv_nexits;
 	int	inv_borders;
-}				t_map_error;
+}				t_map_err;
 
 int		error_msg(char *msg, char *color, char **to_free);
 void	free_matrix(char **to_free);
--- lib/map.h
+++ lib/map.h
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 19:36:06 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/24 13:32:48 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/24 20:55:32 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@
 
 # include "errors.h"
 
-typedef struct s_layout
+typedef struct s_lay
 {
 	int	nrow;
 	int	ncol;
 	int	nexits;
 	int	nplayers;
-	int	ncollectibles;
-}				t_layout;
+	int	ncollect;
+}				t_lay;
 
-void	ft_newlayout(t_layout *lay);
-void	ft_newmap_error(t_map_error *map_error);
-int		ft_print_map_error(t_map_error *map_error);
-int		ft_readlayout(int fd, t_map_error *map_error, t_layout *layout);
-void	ft_check_layout(char *line, t_map_error *map_error, t_layout *layout);
+void	ft_newlayout(t_lay *lay);
+void	ft_newmap_error(t_map_err *map_err);
+int		ft_print_map_error(t_map_err *map_err);
+int		ft_readlayout(int fd, t_map_err *map_err, t_lay *lay);
+void	ft_checklayout(char *line, t_map_err *map_err, t_lay *lay, int old_len);
 
 #endif
