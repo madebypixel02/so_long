1a9f25019e9665965b3a9c41d7cb081f4688b743
madebypixel02 <madebypixel02@gmail.com>
2021-07-26 21:08:00 +0200

    Added direction enum (NORTH, SOUTH, EAST, WEST, STOP)

--- lib/vector.h
+++ lib/vector.h
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 18:56:18 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/26 18:57:45 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/26 20:50:40 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,14 @@ typedef struct s_vector
 	int	x;
 	int	y;
 }				t_vector;
+
+typedef enum s_direction
+{
+	STOP = 0,
+	NORTH = 1,
+	SOUTH = -1,
+	EAST = 2,
+	WEST = -2
+}			t_direction;
 
 #endif
