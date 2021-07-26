982ddca9316fdd1c893a4dc7ecacd7fd46386980
madebypixel02 <madebypixel02@gmail.com>
2021-07-26 14:20:16 +0200

    [WIP] Main now checks for errors and then builds a matrix, freeing it afterwards or as soon as an error occurs

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 16:49:08 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/24 21:08:25 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/26 14:15:36 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,21 @@ int	main(int argc, char **argv)
 	int			fd;
 	t_map_err	map_err;
 	t_lay		lay;
+	char		*map_str;
+	char		**map;
 
 	ft_newlayout(&lay);
 	ft_newmap_error(&map_err);
 	if (argc != 2)
-		return (error_msg("Invalid number of arguments!", RED, NULL));
+		error_msg("Invalid number of arguments!", RED, NULL);
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
-		return (error_msg("File not found!", RED, NULL));
+		error_msg("File not found!", RED, NULL);
 	if (ft_strrncmp(argv[1], ".ber", 4))
-		return (error_msg("Invalid file type, use .ber!", RED, NULL));
-	return (ft_readlayout(fd, &map_err, &lay));
+		error_msg("Invalid file type, use .ber!", RED, NULL);
+	ft_readlayout(fd, &map_err, &lay, &map_str);
+	ft_print_map_error(&map_err, &map_str);
+	map = ft_split(map_str, '\n');
+	free_matrix(map);
+	return (0);
 }
