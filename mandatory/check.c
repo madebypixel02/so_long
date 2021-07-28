61284ad7efdcef56cf360a0c123348279dfc9d30
madebypixel02 <madebypixel02@gmail.com>
2021-07-28 19:47:11 +0200

    Further code cleanup

--- mandatory/check.c
+++ mandatory/check.c
@@ -6,17 +6,15 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 18:45:11 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/28 19:34:53 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/28 19:47:07 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/check.h"
 
-void	check_params(int argc, char **argv)
+t_tile	**check_params(int argc, char **argv)
 {
 	int		fd;
-	char	*map_str;
-	char	**map;
 
 	if (argc != 2)
 		error_msg_params("Invalid number of arguments!", NULL);
@@ -25,24 +23,30 @@ void	check_params(int argc, char **argv)
 		error_msg_params("File not found!", NULL);
 	if (ft_strrncmp(argv[1], ".ber", 4))
 		error_msg_params("Invalid file type, use .ber!", NULL);
-	map_str = check_map(fd);
-	map = ft_split(map_str, '\n');
-	free(map_str);
-	if (!map)
-		error_msg_params("Memory allocation error!", NULL);
-	free_matrix(&map, 1);
+	return (check_map(fd));
 }
 
-char	*check_map(int fd)
+t_tile	**check_map(int fd)
 {
 	char	*map_str;
+	char	**map;
 	t_err	map_err;
 	t_lay	lay;
+	t_tile	**tilemap;
 
 	map_str = NULL;
 	map_err = ft_newmap_error();
 	lay = ft_newlayout();
 	ft_readlayout(fd, &map_err, &lay, &map_str);
 	ft_print_map_error(&map_err, &map_str);
-	return (map_str);
+	map = ft_split(map_str, '\n');
+	free(map_str);
+	if (!map)
+		error_msg_params("Memory allocation error!", NULL);
+	tilemap = ft_tilemap(map, lay);
+	if (!tilemap)
+		error_msg_params("Memory allocation error!", NULL);
+	else
+		free_tilemap(&tilemap, 1);
+	return (tilemap);
 }
