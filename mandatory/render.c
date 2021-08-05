98382ab77bca115bca90f068177cbfb55fa4f045
madebypixel02 <madebypixel02@gmail.com>
2021-08-05 21:06:55 +0200

    Another attempt to fix overlaps

--- mandatory/render.c
+++ mandatory/render.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 17:09:20 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/08/05 21:02:25 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/08/05 21:06:49 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ void	ft_update_score(t_game *g)
 
 	moves = ft_itoa(g->n_moves);
 	mlx_put_image_to_window(g->id, g->w_id, g->sprites.black_font, \
-					g->width / 2 + 25, g->height - 63);
+					g->width / 2 + 25, g->height - 80);
 	mlx_string_put(g->id, g->w_id, g->width / 2 + 25, g->height - 65, \
 					0xFDD663, moves);
 	free(moves);
