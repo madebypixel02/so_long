ff71f7b0e20cf1642c902d10c2bf160dfb94d2e6
madebypixel02 <madebypixel02@gmail.com>
2021-08-15 11:26:36 +0200

    Remove unused mlx.h in main

--- tests/main.c
+++ tests/main.c
@@ -6,11 +6,10 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 16:49:08 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/08/06 20:13:50 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/08/15 11:26:15 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
 #include <fcntl.h>
 #include "../lib/check.h"
 #include "../lib/game.h"
